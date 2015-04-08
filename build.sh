14352abb8f6746fc36aaa95654b51956034708c5
Jianlong cao <jiancao@cisco.com>
2015-04-08 11:06:03 +0800

    [Feature] first added env

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -x
+vimdir=.jayvim
+
+#install pathogen
+echo "install pathogen"
+mkdir -p ~/${vimdir}/autoload ~/${vimdir}/bundle && \
+          curl -LSso ~/${vimdir}/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo  -e "\"pathogen \n\
+#execute pathogen#infect() \n\
+#syntax on 		  \n\
+#filetype plugin indent on \n" > vimrc
+
+echo install symbolic file
+ln -s vimrc ~/.vimrc
+ln -s .jayvim ~/.vim
+ln -s .jayvim/vimrc.map ~/.vimrc.map
+
+cd ~/${vimdir}/bundle
+echo "install nerdtree"
+git clone git://github.com/scrooloose/nerdtree.git
+
+echo "install leaderf"
+git clone https://github.com/Yggdroot/LeaderF.git
+
+echo "install nerdComment"
+git clone https://github.com/scrooloose/nerdcommenter.git
+
+echo "install syntastic"
+git clone https://github.com/scrooloose/syntastic.git
+
+echo "install neocomplete"
+git clone https://github.com/Shougo/neocomplcache.vim.git
+
+echo "install tagbar"
+git clone https://github.com/majutsushi/tagbar.git
+
+echo "install cscope"
+git clone https://github.com/brookhong/cscope.vim.git
+
---
+++ vimrc
@@ -0,0 +1,8 @@
+"pathogen 
+execute pathogen#infect() 
+syntax on 		  
+filetype plugin indent on 
+set ic
+"load keymap
+source ~/.vimrc.map 
+set tabstop=4 shiftwidth=4 expandtab
